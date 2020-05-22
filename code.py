5164eaa8445ad61b90b28216c484cc2c5e7d9d0a
Benoît Marchal <github@psol.com>
2020-05-22 23:09:50 +0200

    Retimer retimers
    Cleans the state management.
    Implement a real retimer (it restarts during the alarm) although the display needs serious improvement.

--- code.py
+++ code.py
@@ -9,25 +9,27 @@ import digitalio
 import gamepadshift
 import neopixel_write
 from adafruit_display_text import label
-from adafruit_bitmap_font import bitmap_font
+from adafruit_bitmap_font  import bitmap_font
 
 # setup
 
 noto18 = bitmap_font.load_font('/fonts/Noto-18.bdf')
 noto18.load_glyphs('1234567890: '.encode('utf-8'))
 
-neopixel = digitalio.DigitalInOut(board.NEOPIXEL)
+neopixel           = digitalio.DigitalInOut(board.NEOPIXEL)
 neopixel.direction = digitalio.Direction.OUTPUT
+NEOPIXEL_LEN       = 5
 
 light_in = analogio.AnalogIn(board.LIGHT)
+audio    = audioio.AudioOut(board.A0)
 
 pad = gamepadshift.GamePadShift(digitalio.DigitalInOut(board.BUTTON_CLOCK),
                                 digitalio.DigitalInOut(board.BUTTON_OUT),
                                 digitalio.DigitalInOut(board.BUTTON_LATCH))
 
-speaker_enable = digitalio.DigitalInOut(board.SPEAKER_ENABLE)
+speaker_enable           = digitalio.DigitalInOut(board.SPEAKER_ENABLE)
 speaker_enable.direction = digitalio.Direction.OUTPUT
-speaker_enable.value = True
+speaker_enable.value     = True
 
 prev_buttons = pad.get_pressed()
 press_delay  = 0
@@ -43,44 +45,46 @@ BUTTON_START = const(4)
 BUTTON_A     = const(2)
 BUTTON_B     = const(1)
 
+# states
+
+CONFIG         = const(1)
+COUNTDOWN      = const(2)
+ALARM          = const(3)
+ALARM_LOOP     = const(4)
+END            = const(5)
+
 # timers and retimer
 
-timers       = [0, 0, 0]
+timers = [0, 0, 0]
 
-RETIMER   = 0
-TIMER1    = 1
-TIMER2    = 2
-MAX_TIMER = 24 * 60 * 60
+RETIMER   = const(0)
+TIMER1    = const(1)
+TIMER2    = const(2)
+A_TIMER   = const(3)
+MIN_TIMER = const(0)
+MAX_TIMER = const(24 * 60 * 60)
+IDL_TIMER = const(-1)
 
 # utilities
 
-def moving_average(size):
+def light_moving_average(size):
   queue = []
-  avg = None
 
   while True:
-    value = yield avg
-    queue.append(value)
+    value = light_in.value
+    queue.insert(0, value)
     if len(queue) > size:
       queue.pop()
-    avg = sum(queue) / len(queue)
+    yield (value, sum(queue) / len(queue))
 
 
 def hor_min_sec(seconds):
   min = math.floor(seconds / 60)
   return (math.floor(min / 60), min % 60, seconds % 60)
 
-
-def remain_time(t):
-  return -1 if t == 0 else max(0, t - time.time())
-
 # configuration screen
 
-prev_press   = 0
-selection    = 3
-
-
-def display_group_config(timers, selection):
+def config_screen(timers, selection):
   def row(seconds, y, local_selection):
     hor, min, sec = hor_min_sec(seconds)
 
@@ -109,8 +113,8 @@ def display_group_config(timers, selection):
   return result
 
 
-def read_config_buttons():
-  global pad, prev_buttons, press_delay, prev_press, timers, selection
+def config_loop(timers = [0, 0, 0], selection = 3):
+  global pad
 
   def inc_by(amount):
     if selection in (3, 6, 9): # seconds
@@ -122,48 +126,79 @@ def read_config_buttons():
     else:
       assert False
 
-  buttons = pad.get_pressed()
+  # initially request a display
+  board.DISPLAY.show(config_screen(timers, selection))
 
-  increment = 0
-  if prev_press == 0:
-    if buttons != prev_buttons:
-      increment = inc_by(1)
-      prev_press = 1
-  elif time.monotonic() > press_delay and buttons == prev_buttons:
-    if prev_press == 1:
-      increment = inc_by(4)
-    elif prev_press in (2, 3):
-      increment = inc_by(5)
-    elif prev_press > 3:
-      increment = inc_by(10)
-    if prev_press < 4:
-      prev_press += 1
-  elif buttons != prev_buttons:
-    prev_press = 0
-  prev_buttons = buttons
+  prev_buttons = 0
+  repeat_count = 0
+  repeat_delay = 0
 
-  if increment != 0:
-    press_delay = time.monotonic() + 0.5
-    if (buttons & BUTTON_RIGHT) > 0:
-      selection = min(9, selection + 1)
-    elif (buttons & BUTTON_LEFT) > 0:
-      selection = max(1, selection - 1)
-    elif (buttons & BUTTON_UP) > 0 or (buttons & BUTTON_DOWN) > 0:
-      if (buttons & BUTTON_DOWN) > 0:
-        increment = -increment
-      selected_timer = math.floor((selection - 1) / 3)
-      timers[selected_timer] = min(MAX_TIMER, max(0, timers[selected_timer] + increment))
-    elif (buttons & BUTTON_START) > 0:
-      return -1
-    return 1
-  else:
-    return 0
+  while True:
+    buttons = pad.get_pressed()
 
-# countdown screen
+    increment = 0
+    if repeat_count == 0:
+      if buttons != prev_buttons:
+        increment = inc_by(1)
+        repeat_count = 1
+        repeat_delay = time.monotonic() + 0.5
+    elif time.monotonic() > repeat_delay and buttons == prev_buttons:
+      if repeat_count == 1:
+        increment = inc_by(4)
+      elif repeat_count in (2, 3):
+        increment = inc_by(5)
+      elif repeat_count > 3:
+        increment = inc_by(10)
+      repeat_count = max(4, repeat_count + 1)
+      repeat_delay = time.monotonic() + 0.5
+    elif buttons != prev_buttons:
+      repeat_count = 0
+      repeat_delay = 0
+    prev_buttons = buttons
 
-tick_delay = 0
+    if increment != 0:
+      if buttons & BUTTON_RIGHT > 0:
+        selection = min(9, selection + 1)
+      elif buttons & BUTTON_LEFT > 0:
+        selection = max(1, selection - 1)
+      elif buttons & BUTTON_UP > 0 or buttons & BUTTON_DOWN > 0:
+        if buttons & BUTTON_DOWN > 0:
+          increment = -increment
+        selection_timer = math.floor((selection - 1) / 3)
+        timers[selection_timer] = min(MAX_TIMER, max(MIN_TIMER, timers[selection_timer] + increment))
+      if buttons & BUTTON_START > 0:
+        return (COUNTDOWN, timers)
+      else:
+        board.DISPLAY.show(config_screen(timers, selection))
 
-def display_group_countdown(timers, tick):
+# countdown (and alarm) screen
+
+Blank_np = bytearray([0, 0, 0] * NEOPIXEL_LEN)
+Retimer_np = bytearray([200, 0, 100] * NEOPIXEL_LEN)
+Timer_np = bytearray([150, 150, 0] * NEOPIXEL_LEN)
+
+def alarm_np(target):
+  while True:
+    for count in range(3):
+      if count == 0:
+        yield Retimer_np if target == RETIMER else Timer_np
+      else:
+        yield Blank_np
+
+
+def alarm_audio():
+  global audio
+
+  tone_volume = 1  # Increase this to increase the volume of the tone.
+  frequency = 440  # Set this to the Hz of the tone you want to generate.
+  length = 8000 // frequency
+  sine_wave = array.array("H", [0] * length)
+  for i in range(length):
+    sine_wave[i] = int((1 + math.sin(math.pi * 2 * i / length)) * tone_volume * (2 ** 15 - 1))
+  return audioio.RawSample(sine_wave)
+
+
+def countdown_screen(timers, tick):
   def row(seconds, y, active):
     hor, min, sec = hor_min_sec(seconds)
     if tick:
@@ -176,137 +211,93 @@ def display_group_countdown(timers, tick):
     text_area.text = template.format(hor, min, sec)
     return text_area
 
+  timers = list(timers)
+  print('repaint {}'.format(','.join(str(t) for t in timers)))
   result = displayio.Group()
   for i, timer in enumerate(timers):
     result.append(row(max(0, timer), (i + 1) * 30, timer >= 0))
   return result
 
 
-def read_countdown_buttons():
-  global pad, prev_buttons, tick_delay, press_delay
-
-  result = 0
-
-  buttons = pad.get_pressed()
-  if buttons != prev_buttons:
-    if (buttons & BUTTON_SEL) > 0:
-      result = -1
-    press_delay = time.monotonic() + 0.5
-  prev_buttons = buttons
-
-  if time.time() > tick_delay:
-    result = 1
-    tick_delay = time.time()
-  
+def re_timers(retimers, timers, curr):
+  result = []
+  for i, t in enumerate(retimers):
+    if i == RETIMER:
+      print(t)
+      result.append(timers[i])
+    else:
+      result.append(curr(t))
   return result
 
-# alarm
 
-light_average = moving_average(5)
-next(light_average)  # prime coroutine
-compare_avg = -1
-stop_at = 0
-compare_at = float('inf')
-audio = audioio.AudioOut(board.A0)
+def countdown_loop(timers):
+  global pad
 
-def start_alarm():
-  global audio, stop_at
+  start_time = time.time()
+  retimers = timers
 
-  tone_volume = 1  # Increase this to increase the volume of the tone.
-  frequency = 440  # Set this to the Hz of the tone you want to generate.
-  length = 8000 // frequency
-  sine_wave = array.array("H", [0] * length)
-  for i in range(length):
-    sine_wave[i] = int((1 + math.sin(math.pi * 2 * i / length)) * tone_volume * (2 ** 15 - 1))
-  sine_wave_sample = audioio.RawSample(sine_wave)
-  stop_at = time.monotonic() + 300
-  audio.play(sine_wave_sample, loop=True)
+  def current():
+    from_start_time = time.time() - start_time
+    return lambda t: max(MIN_TIMER, t - from_start_time)
 
+  def idle_or_current():
+    curr = current()
+    return lambda t: IDL_TIMER if t == 0 else curr(t)
 
-def read_alarm_light():
-  global compare_avg, compare_at, light_average, audio, stop_at
-  
-  if time.monotonic() > stop_at:
-    neopixel_write.neopixel_write(neopixel, bytearray([i for p in range(0, 5) for i in (0, 0, 0)]))
-    return -1
-  elif time.monotonic() > compare_at:
-    value = light_in.value
-    if value < compare_avg:
-      neopixel_write.neopixel_write(neopixel, bytearray([i for p in range(0, 5) for i in (255, 0, 0)]))
-      time.sleep(0.3)
-      neopixel_write.neopixel_write(neopixel, bytearray([i for p in range(0, 5) for i in (0, 0, 0)]))
-      return -1
-    return 0
-  elif compare_avg < 0:
-    value = light_in.value
-    avg = light_average.send(value)
-    if value < avg * 0.66:
-      audio.stop()
-      neopixel_write.neopixel_write(neopixel, bytearray([i for p in range(0, 5) for i in (0, 0, 255)]))
-      compare_avg = avg * 0.66
-      compare_at = time.monotonic() + 0.3
-    return 0
-  elif (pad.get_pressed() & BUTTON_START) > 0:
-    neopixel_write.neopixel_write(neopixel, bytearray([i for p in range(0, 5) for i in (0, 0, 0)]))
-    audio.stop()
-    return -1
-  else:
-    return 0
+  alarm = None
+  prev_buttons = 0
+  tick_delay = float('-inf')
+  light_average = light_moving_average(5)
 
+  while True:
+    restart_btn = False
+    value, avg = next(light_average)
 
-def play_alarm():
-  sec = prev_sec = time.time()
-  for i in range(100):
-    value = light_in.value
-    avg = light_average.send(value)
-    if value < (avg * 0.66):
-      audio.stop()
-      neopixel_write.neopixel_write(neopixel, bytearray([i for p in range(0, 5) for i in (0, 0, 255)]))
-      time.sleep(2)
-      break
-    sec = time.time()
-    if sec != prev_sec:
-      colour = (0, 255, 0)
+    buttons = pad.get_pressed()
+    if buttons != prev_buttons:
+      prev_buttons = buttons
+      if buttons & BUTTON_SEL > 0:
+        return (CONFIG, list(map(current(), retimers)))
+      else:
+        restart_btn = buttons & BUTTON_A > 0 or buttons & BUTTON_B > 0
+
+    ioc = idle_or_current()
+    if any(ioc(t) == MIN_TIMER for t in retimers):
+      board.DISPLAY.show(countdown_screen(map(ioc, retimers), True))
+      audio.play(alarm_audio(), loop=True)
+      if alarm == None:
+        if ioc(retimers[RETIMER]) == MIN_TIMER:
+          alarm = alarm_np(RETIMER)
+        else:
+          alarm = alarm_np(A_TIMER)
+      retimers = re_timers(retimers, timers, current())
+      start_time = time.time()
+      print(retimers)
+
+    if alarm != None:
+      if restart_btn or value < avg * 0.66:
+        alarm = None
+        audio.stop()
+        neopixel_write.neopixel_write(neopixel, Blank_np)
+        if all(t == 0 for t in retimers[1:]):
+          return (CONFIG, timers)
+      else:
+        neopixel_write.neopixel_write(neopixel, next(alarm))
+    elif time.time() > tick_delay:
+      tick_delay = time.time()
+      board.DISPLAY.show(countdown_screen(map(ioc, retimers), time.time() % 2))
+      
+    if alarm == None:
+      time.sleep(1)
     else:
-      colour = (0, 0, 0)
-    prev_sec = sec
-    neopixel_write.neopixel_write(neopixel, bytearray([i for p in range(0, 5) for i in colour]))
-    time.sleep(0.1)
+      time.sleep(0.3)
 
 # main
 
-board.DISPLAY.show(display_group_config(timers, selection))
-action = read_config_buttons() 
-state = 0
-until_timers = [0] * len(timers)
+state = CONFIG
 
-while state >= -1:
-  if state == 0:
-    if action > 0:
-      board.DISPLAY.show(display_group_config(timers, selection))
-    if action < 0:
-      state = 1
-      until_timers = list(map(lambda t: 0 if t == 0 else time.time() + t, timers))
-      action = read_countdown_buttons()
-    else:
-      action = read_config_buttons()
-  elif state == 1:
-    if action > 0:
-      now = time.time()
-      if all(now > t for t in until_timers):
-        start_alarm()
-        state = -1
-        action = read_alarm_light()
-      board.DISPLAY.show(display_group_countdown(map(remain_time, until_timers), time.time() % 2))
-      continue
-    if action < 0:
-      state = 0
-      action = read_config_buttons()
-      board.DISPLAY.show(display_group_config(timers, selection))
-    else:
-      action = read_countdown_buttons()
-  elif state == -1:
-    if action == 0:
-      action = read_alarm_light()
-    elif action < 0:
-      state = -2
+while state != END:
+  if state == CONFIG:
+    state, timers = config_loop(timers)
+  elif state == COUNTDOWN:
+    state, timers = countdown_loop(timers)
