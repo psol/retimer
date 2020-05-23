1b45b58453805945d3d7e5c58e13eb80a3cde505
Benoît Marchal <github@psol.com>
2020-05-23 21:44:10 +0200

    Minimal set of features
    First version that implements the minimal set of features: retimer, timer 1 and timer 2.

--- README.md
+++ README.md
@@ -1,12 +1,91 @@
 # retimer
-When I cook and particularly when I cook vegetables, I need a regular reminder to check
-on the food. Enter the retimer.
+When I cook and particularly when I grill vegetables, I need a regular reminder to check
+on the food. Enter the retimer. The retimer is a cooking timer that re-triggers every
+x minutes to check on the grilling and, usually, toss the vegetables around.
 
-The retimer is (or will be) a cooking timer that re-triggers every x minutes to go and
-check on the food cooking.
+Same thing with no-knead bread who needs a fold every 20-25 minutes.
 
-## PyBadge
-retimer is currently developed for the Adafruit PyBadge, in CircuitPython 5.3.0.
-The following libraries are used:
+For convenient, two regular timers are also supported, typically timer 1 goes off when
+cooking should be finished and timer 2 a little bit earlier to add aromatics.
+For bread-making, timer 1 goes off when the dough should be proofed while timer 2
+is shaping time.
+
+Last but not least, I wanted hands-free control of the retimer. Obstructing the light
+sensor is equivalent to pressing button A (restart).
+
+## UX Prototype
+A kitchen timer is not a particulary difficult project. In fact I have had a similar
+workflow for some time: I set my kitchen timer to 5 minutes and when it goes off,
+I verify my grilling and restart the timer.
+
+Expect I sometimes forget to restart the timer.
+
+OKay, I often forget to restart the timer.
+
+So the retimer aims to achieve a better UX for the task of grilling vegetables.
+When designing UX, it's important to formulate hypothesis and validate them through
+prototypes. Paper prototypes would not real work here so I had to build something fast.
+Which leads us to hardware.
+
+## Hardware
+retimer is developed on the [Adafruit PyBadge](https://www.adafruit.com/product/4200),
+in [CircuitPython 5.3.0](https://circuitpython.org).
+
+The PyBadge has plenty of buttons, a light sensor (hands-free operation), a screen,
+neopixels, on-board audio and sophisticated power management that includes a battery
+connector. It is also robust (mine has survived a few drops). All this makes it a great
+prototyping board. 
+
+Unfortunately I have found that the PyBadge is not Arduino-friendly. At least not friendly
+enough to my liking. It is certainly possible to develop in Arduino but I had to reset
+the board before every upload… which is a pain. So CircuitPython it was, plus Python
+is a great tool for rapid prototyping.
+
+Unfortunately CircuitPython cannot handle RTC interrupts (according to the current FAQ)
+or allow the board to go to sleep, which makes it power hungry for a kitchen timer.
+
+### PyBadge or PyBadge LC
+
+I used a plain PyBadge but the PyBadge LC should work also it is less expensive.
+The PyBadge LC has only one NeoPixel (adjust `NEOPIXEL_LEN`) and is limited to the onboard
+speaker.
+
+## Libraries
+The following [libraries](https://circuitpython.org/libraries) are also used:
 * adafruit_bitmap_font
 * adafruit_display_text
+
+The font [Noto](https://www.google.com/get/noto/) is from Google. I retrieved
+the BDF file from the
+[PyBadge Conference Badge project](https://github.com/adafruit/Adafruit_Learning_System_Guides)
+
+## How to use?
+At launch, the board shows the configuration screen. Set any of the 3 timers with
+the d-pad. Timers you don't need can be left to zero and they will not trigger an alarm.
+To start the countdown, hit the _Start_ button.
+
+While the timer is counting down, the _Select_ button returns to the configuration screen.
+
+When one of the timers reaches zero, the alarm goes off. Pressing the _A_ (or _B_) button
+or momentarily obstructing the light sensor stops the alarm. If more than one timer has
+been selected, the alarm does not stop them. The retimer also restarts automatically,
+(unless it was set to zero).
+
+The _B_ button behaves differently for the retimer. It stops the retimer, so for timer 1
+and timer 2, you can use either button indifferently. For the retimer button A stops
+the alarm but restart the timer. Button B stops the alarm and returns to the configuration
+screen.
+
+## A good trade-off
+Using a generic board, like the PyBadge, has many benefits in terms of "time to market"
+but it's a little bit of a trade-off.
+
+I would be interested to try rotary encoders as an alternative to the d-pad and
+the start/stop buttons. A gesture sensor, such as the APDS-9960, would expand options
+for hands-free operations, always a good thing in the kitchen.
+
+It would be interested to connect to a thermometer or another sensor, maybe through
+Bluetooth. And I have already discussed power saving.
+
+Yet overal I'm happy with the trade-off. At minimum, I have a prototype to validate
+in the kitchen. Later I'll see whether I need to improve.
--- code.py
+++ code.py
@@ -6,12 +6,13 @@ import audioio
 import analogio
 import displayio
 import digitalio
+import terminalio
 import gamepadshift
 import neopixel_write
 from adafruit_display_text import label
 from adafruit_bitmap_font  import bitmap_font
 
-# setup
+# setup GPIO and font
 
 noto18 = bitmap_font.load_font('/fonts/Noto-18.bdf')
 noto18.load_glyphs('1234567890: '.encode('utf-8'))
@@ -31,9 +32,6 @@ speaker_enable           = digitalio.DigitalInOut(board.SPEAKER_ENABLE)
 speaker_enable.direction = digitalio.Direction.OUTPUT
 speaker_enable.value     = True
 
-prev_buttons = pad.get_pressed()
-press_delay  = 0
-
 # button constants
 
 BUTTON_LEFT  = const(128)
@@ -45,31 +43,34 @@ BUTTON_START = const(4)
 BUTTON_A     = const(2)
 BUTTON_B     = const(1)
 
-# states
+# state constants
 
 CONFIG         = const(1)
 COUNTDOWN      = const(2)
-ALARM          = const(3)
-ALARM_LOOP     = const(4)
-END            = const(5)
+END            = const(3) # currently not used
 
-# timers and retimer
+# colour constants
 
-timers = [0, 0, 0]
+C_DEFAULT  = const(0x2222ff)
+C_DISABLED = const(0x555555)
+C_SELECTED = const(0xff5500)
 
-RETIMER   = const(0)
-TIMER1    = const(1)
-TIMER2    = const(2)
-A_TIMER   = const(3)
-MIN_TIMER = const(0)
-MAX_TIMER = const(24 * 60 * 60)
-IDL_TIMER = const(-1)
+# timers and retimer constants
+
+RETIMER     = const(0)
+TIMER1      = const(1)
+TIMER2      = const(2)
+TIMERS_LEN  = const(3)
+NOT_RETIMER = const(3)
+MIN_TIMER   = const(0)
+MAX_TIMER   = const(24 * 60 * 60)
+IDL_TIMER   = const(-1)
 
 # utilities
 
 def light_moving_average(size):
+  global light_in
   queue = []
-
   while True:
     value = light_in.value
     queue.insert(0, value)
@@ -82,34 +83,42 @@ def hor_min_sec(seconds):
   min = math.floor(seconds / 60)
   return (math.floor(min / 60), min % 60, seconds % 60)
 
+
+def labels(active = [True, True, True]):
+  result = displayio.Group(max_size=TIMERS_LEN)
+  for i, text in enumerate(('retimer', 'timer 1', 'timer 2')):
+    text_area = label.Label(terminalio.FONT, text=text, color=C_DEFAULT, x=110, y=(i + 1) * 35)
+    text_area.color = C_DEFAULT if active[i] else C_DISABLED
+    result.append(text_area)
+  return result
+
 # configuration screen
 
 def config_screen(timers, selection):
   def row(seconds, y, local_selection):
+    global noto18
     hor, min, sec = hor_min_sec(seconds)
-
-    labels = (
+    texts = (
       ('{:02d}'.format(hor), 1, 1),
       (':', None, 3),
       ('{:02d}'.format(min), 2, 1),
       (':', None, 3),
       ('{:02d}'.format(sec), 3, 1)
     )
-
     x = 10
     result = displayio.Group(max_size=5)
-    for st, pos, kern in labels:
-      text_label = label.Label(noto18, text=st ,x=x, y=y)
-      text_label.color = 0xff5500 if local_selection == pos else 0x0000ff
+    for txt, pos, kern in texts:
+      text_label = label.Label(noto18, text=txt ,x=x, y=y)
+      text_label.color = C_SELECTED if local_selection == pos else C_DEFAULT
       result.append(text_label)
       bx, by, w, h = text_label.bounding_box
       x += w + kern
-
     return result
 
-  result = displayio.Group()
+  result = displayio.Group(max_size=TIMERS_LEN + 1)
+  result.append(labels())
   for i, timer in enumerate(timers):
-    result.append(row(timer, (i + 1) * 30, selection - (i * 3)))
+    result.append(row(timer, (i + 1) * 35, selection - (i * 3)))
   return result
 
 
@@ -126,17 +135,15 @@ def config_loop(timers = [0, 0, 0], selection = 3):
     else:
       assert False
 
-  # initially request a display
+  # initial config screen
   board.DISPLAY.show(config_screen(timers, selection))
-
   prev_buttons = 0
   repeat_count = 0
   repeat_delay = 0
-
   while True:
     buttons = pad.get_pressed()
-
     increment = 0
+    # interpret button press, with logic to increment faster if a button is held down
     if repeat_count == 0:
       if buttons != prev_buttons:
         increment = inc_by(1)
@@ -155,7 +162,7 @@ def config_loop(timers = [0, 0, 0], selection = 3):
       repeat_count = 0
       repeat_delay = 0
     prev_buttons = buttons
-
+    # update the values and repaint the screen, if needed
     if increment != 0:
       if buttons & BUTTON_RIGHT > 0:
         selection = min(9, selection + 1)
@@ -173,22 +180,19 @@ def config_loop(timers = [0, 0, 0], selection = 3):
 
 # countdown (and alarm) screen
 
-Blank_np = bytearray([0, 0, 0] * NEOPIXEL_LEN)
+Blank_np   = bytearray([0, 0, 0] * NEOPIXEL_LEN)
 Retimer_np = bytearray([200, 0, 100] * NEOPIXEL_LEN)
-Timer_np = bytearray([150, 150, 0] * NEOPIXEL_LEN)
+Timer_np   = bytearray([150, 150, 0] * NEOPIXEL_LEN)
+
 
 def alarm_np(target):
   while True:
-    for count in range(3):
-      if count == 0:
-        yield Retimer_np if target == RETIMER else Timer_np
-      else:
-        yield Blank_np
+    yield Retimer_np if target == RETIMER else Timer_np
+    yield Blank_np
+    yield None
 
 
 def alarm_audio():
-  global audio
-
   tone_volume = 1  # Increase this to increase the volume of the tone.
   frequency = 440  # Set this to the Hz of the tone you want to generate.
   length = 8000 // frequency
@@ -198,103 +202,137 @@ def alarm_audio():
   return audioio.RawSample(sine_wave)
 
 
+def clear_alarm():
+  global audio, neopixel
+  audio.stop()
+  neopixel_write.neopixel_write(neopixel, Blank_np)
+
+
 def countdown_screen(timers, tick):
-  def row(seconds, y, active):
+  DEFAULT  = const(0)
+  SELECTED = const(1)
+  DISABLED = const(2)
+  Colours = [C_DEFAULT, C_SELECTED, C_DISABLED]
+
+  def row(seconds, y, flag):
+    global noto18
     hor, min, sec = hor_min_sec(seconds)
     if tick:
       template = '{:02d}:{:02d}:{:02d}'
     else:
       template = '{:02d} {:02d} {:02d}'
-
     text_area = label.Label(noto18, max_glyphs=8, x=10, y=y)
-    text_area.color = 0x0000ff if active else 0x555555
+    text_area.color = Colours[state]
     text_area.text = template.format(hor, min, sec)
     return text_area
 
-  timers = list(timers)
-  print('repaint {}'.format(','.join(str(t) for t in timers)))
-  result = displayio.Group()
+  result = displayio.Group(max_size=TIMERS_LEN + 1)
+  active = []
   for i, timer in enumerate(timers):
-    result.append(row(max(0, timer), (i + 1) * 30, timer >= 0))
-  return result
-
-
-def re_timers(retimers, timers, curr):
-  result = []
-  for i, t in enumerate(retimers):
-    if i == RETIMER:
-      print(t)
-      result.append(timers[i])
+    active.append(timer >= 0)
+    if timer == 0:
+      state = SELECTED
+    elif timer > 0:
+      state = DEFAULT
     else:
-      result.append(curr(t))
+      state = DISABLED
+    result.append(row(max(0, timer), (i + 1) * 35, state))
+  result.append(labels(active))
   return result
 
 
 def countdown_loop(timers):
-  global pad
+  global pad, audio, neopixel
 
-  start_time = time.time()
-  retimers = timers
-
-  def current():
+  def idle_or_current(start_time):
     from_start_time = time.time() - start_time
-    return lambda t: max(MIN_TIMER, t - from_start_time)
+    current = lambda t: max(MIN_TIMER, t - from_start_time)
+    return (current, lambda t: IDL_TIMER if t == 0 else current(t))
 
-  def idle_or_current():
-    curr = current()
-    return lambda t: IDL_TIMER if t == 0 else curr(t)
+  # since there are 2 timers and the retimer, when one of them stops, we need specify
+  # logic to compute how much time is left on the others
+  def re_timers(retimers, timers, curr):
+    result = []
+    for i, t in enumerate(retimers):
+      if i == RETIMER and curr(retimers[RETIMER]) == 0:
+          result.append(timers[i])
+      else:
+        result.append(curr(t))
+    return (time.time(), result)
 
   alarm = None
   prev_buttons = 0
+  retimers = timers
+  configs = list(timers)
+  start_time = time.time() # when the countdown starts
   tick_delay = float('-inf')
   light_average = light_moving_average(5)
-
   while True:
-    restart_btn = False
-    value, avg = next(light_average)
-
+    stop_retimer = stop_timer = False
+    current, ioc = idle_or_current(start_time)
+    light, avg = next(light_average)
+    # interpret button press or light-based gesture
     buttons = pad.get_pressed()
     if buttons != prev_buttons:
       prev_buttons = buttons
       if buttons & BUTTON_SEL > 0:
-        return (CONFIG, list(map(current(), retimers)))
+        clear_alarm()
+        return (CONFIG, list(map(current, retimers)))
       else:
-        restart_btn = buttons & BUTTON_A > 0 or buttons & BUTTON_B > 0
-
-    ioc = idle_or_current()
-    if any(ioc(t) == MIN_TIMER for t in retimers):
-      board.DISPLAY.show(countdown_screen(map(ioc, retimers), True))
-      audio.play(alarm_audio(), loop=True)
-      if alarm == None:
+        stop_timer = buttons & BUTTON_A > 0
+        stop_retimer  = buttons & BUTTON_B > 0
+    stop_timer = stop_timer or light < avg * 0.66
+    if alarm == None:
+      # alarm currently off, should we start it?
+      if any(ioc(t) == MIN_TIMER for t in retimers):
+        audio.play(alarm_audio(), loop=True)
         if ioc(retimers[RETIMER]) == MIN_TIMER:
           alarm = alarm_np(RETIMER)
         else:
-          alarm = alarm_np(A_TIMER)
-      retimers = re_timers(retimers, timers, current())
-      start_time = time.time()
-      print(retimers)
-
-    if alarm != None:
-      if restart_btn or value < avg * 0.66:
+          alarm = alarm_np(NOT_RETIMER)
+    else:
+      # alarm ongoing, stop it if...
+      if stop_timer or stop_retimer:
         alarm = None
-        audio.stop()
-        neopixel_write.neopixel_write(neopixel, Blank_np)
-        if all(t == 0 for t in retimers[1:]):
-          return (CONFIG, timers)
+        clear_alarm()
+        # ...the stop button is pressed for the retimer...
+        if stop_retimer and ioc(retimers[RETIMER]) == MIN_TIMER:
+          # ...timer1 or timer2 still ongoing, stops only the retimer...
+          if any(ioc(t) > 0 for t in retimers[1:]):
+            timers[RETIMER] = 0
+            start_time, retimers = re_timers(retimers, timers, current)
+            curr, ioc = idle_or_current(start_time)
+          # ...timer1 and timer2 also stopped, returns to config...
+          else:
+            return (CONFIG, configs)
+        # ...else it's for timer1, timer2 or it's the continue button for retimer...
+        else:
+          # ...we restart a countdown (timer1 or timer2 still counting or retimer restarts)...
+          if ioc(retimers[RETIMER]) >= 0 or any(ioc(t) > 0 for t in retimers[1:]):
+            start_time, retimers = re_timers(retimers, timers, current)
+            curr, ioc = idle_or_current(start_time)
+          # ...but we stop if all timers are over
+          else:
+            return (CONFIG, configs)
       else:
-        neopixel_write.neopixel_write(neopixel, next(alarm))
-    elif time.time() > tick_delay:
+      # alarm ongoing, not stopped so animate the neopixels
+        colour = next(alarm)
+        if colour != None:
+          neopixel_write.neopixel_write(neopixel, colour)
+    # repaint the screen (roughly) every second so the ':' ticks in and off
+    if time.time() > tick_delay:
       tick_delay = time.time()
-      board.DISPLAY.show(countdown_screen(map(ioc, retimers), time.time() % 2))
-      
+      board.DISPLAY.show(countdown_screen(map(ioc, retimers), time.time() % 2))      
     if alarm == None:
       time.sleep(1)
     else:
+      # shorter sleep to support alarm animation
       time.sleep(0.3)
 
 # main
 
 state = CONFIG
+timers = [0, 0, 0]
 
 while state != END:
   if state == CONFIG:
